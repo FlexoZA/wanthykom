c0f7a072f0adbd68bb4cc79503c9e00e74ce45fe
Christiaan <christiaan@schoeman.me>
2025-08-07 08:11:06 +0200

    Add todo of sort_order

--- src/stores/web/supabaseBookStore.js
+++ src/stores/web/supabaseBookStore.js
@@ -32,31 +32,35 @@ export const useSupabaseBookStore = defineStore('supabaseBook', {
             `
             book_name,
             book_image_url,
+            created_at,
             book_header (
                 book_header_name,
                 book_header_text,
                 book_header_image_url,
+                created_at,
                 enable
             ),
             chapter (
                 chapter_name,
                 chapter_text,
                 book_chapter_image_url,
+                created_at,
                 enable
             )
           `,
           )
-          .order('created_at', { ascending: true })
+          .order('created_at', { ascending: false })
 
         if (error) {
           console.error('DEBUG::supabaseBookStore', 'Error fetching Books:', error)
           throw error
         }
 
-        // Set the books data, filter enabled items, and sort chapters
+        // Set the books data, filter enabled items, and sort chapters and headers by newest first
         this.books = data.map(book => ({
           ...book,
-          book_header: book.book_header?.filter(header => header.enable === true) || [],
+          book_header: book.book_header?.filter(header => header.enable === true)
+            .sort((a, b) => new Date(b.created_at) - new Date(a.created_at)) || [],
           chapter: book.chapter?.filter(chapter => chapter.enable === true)
             .sort((a, b) => new Date(b.created_at) - new Date(a.created_at)) || []
         }))
